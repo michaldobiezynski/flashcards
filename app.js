f57ac03cf7ecedc2d2ccb80de8a572d17cf7868d
michaldobiezynski <dobiezynski@gmail.com>
2019-11-01 19:17:54 +0000

    parsing info from the form

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({extended:false}));
 
 app.set('view engine', 'pug');
 
@@ -19,7 +21,7 @@ app.get('/hello', (req, res) => {
 })
 
 app.post('/hello', (req, res) => {
-    console.dir(req);
+    console.dir(req.body);
     res.render('hello.pug');
 })
 
