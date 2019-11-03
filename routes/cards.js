c0634969113b62b93798dcc20985b693ec91a916
michaldobiezynski <dobiezynski@gmail.com>
2019-11-03 19:19:08 +0000

    added a variable to the cards route to didplay different cards

--- routes/cards.js
+++ routes/cards.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const { data } = require('../data/flashcardData.json');
 const { cards } = data;
 
-router.get('/', (req, res) => {
+router.get('/:id', (req, res) => {
     // res.locals.prompt = "Who is buried in Grant's tomb?";
     res.render('card.pug', {
-        prompt: cards[0].question,
-        hint: cards[0].hint
+        prompt: cards[req.params.id].question,
+        hint: cards[req.params.id].hint
     });
 });
 
