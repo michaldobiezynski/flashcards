59562bfb1145064c864a227591316780ae09006e
michaldobiezynski <dobiezynski@gmail.com>
2019-11-03 19:11:23 +0000

    pulling and using data from flat data file

--- routes/cards.js
+++ routes/cards.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { data } = require('../data/flashcardData');
+const { data } = require('../data/flashcardData.json');
 const { cards } = data;
 
 router.get('/', (req, res) => {
     // res.locals.prompt = "Who is buried in Grant's tomb?";
-    res.render('card.pug', {prompt: "Who is buried in Grant's tomb?"});
+    res.render('card.pug', {
+        prompt: cards[0].question,
+        hint: cards[0].hint
+    });
 });
 
 module.exports = router;
